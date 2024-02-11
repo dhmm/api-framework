fdd76770316a25e8813b105615dd5ee37faef893
dhmm <dhmm@outlook.com>
2024-02-11 15:17:47 +0200

    first structure ready

---
+++ core/framework/framework.ts
@@ -0,0 +1,9 @@
+class Framework
+{
+	version = '1.0.0'
+	start() {
+		console.log('Starting framework '+this.version);
+	}
+}
+
+export default Framework;
---
+++ index.ts
@@ -0,0 +1,5 @@
+import Framework from "./core/framework/framework";
+
+
+const framework = new Framework()
+framework.start();
