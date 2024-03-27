f01eabfb1b161c1c4844f3f7814357e00892db86
dhmm <dhmm@outlook.com>
2024-03-27 11:13:06 +0200

    cntrollers laoder removed from the laoders of the framewrok

---
+++ src/app/controllers/Test.ts
@@ -0,0 +1,11 @@
+class Test
+{
+    //test/route1
+    route1() {
+
+    }
+    //test/route2
+    route2() {
+
+    }
+}
--- src/system/framework/framework.ts
+++ src/system/framework/framework.ts
@@ -1,26 +1,22 @@
 //File : system/framework/framework.ts
 import SystemConstants from "../constants/system";
 import ConfigLoader from "../loaders/config-loader";
-import ControllersLoader from "../loaders/controllers-loader";
 import Server from "../server/server";
 import clog from "../utils/clog/clog";
 
 class Framework
 {
 	pathsConfig: any = null;
-  envConfig: any = null;
-	server = new Server();
-	controllers:any = null;
+    envConfig: any = null;
+	server = new Server();	
 
 	async start() {		
 		clog.magenta('Starting framework version : '+ SystemConstants.SystemVersion);		
 		this.pathsConfig = ConfigLoader.loadPathsConfig();
 		clog.yellow(this.pathsConfig);
     try {
-      let jsonConfig = await ConfigLoader.loadEnvConfig(this.pathsConfig);
-      this.envConfig = JSON.parse(jsonConfig);  
-			
-			this.controllers = await ControllersLoader.loadControllers(this.pathsConfig, this.envConfig);
+        let jsonConfig = await ConfigLoader.loadEnvConfig(this.pathsConfig);
+        this.envConfig = JSON.parse(jsonConfig);  					
     }
     catch {
       this.envConfig = null;
